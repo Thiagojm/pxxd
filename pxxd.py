dbc24140377b07535fae8c0c0bdc70550e7285ea
Thiago <tjm.plastica@gmail.com>
2019-09-29 00:49:10 -0300

    oi

--- pxxd.py
+++ pxxd.py
@@ -1,21 +1,103 @@
 from bitstring import BitArray, BitStream
 from textwrap import wrap
 import timeit
+import pandas as pd
 
-def code():
-    with open('teste.bin', "rb") as file: # open binary file
+
+global binFile
+global num_ones_array
+
+
+def fileBinToAscii():
+    global num_ones_array
+    global binFile
+    num_ones_array = []
+    with open('file.bin', "rb") as file: # open binary file
         bin_hex = BitArray(file) # bin to hex
     bin_ascii = bin_hex.bin
-    print("Here are the bits: " + str(bin_ascii))
+    #print("Here are the bits: " + str(bin_ascii))
     total_bits = len(bin_ascii)
-    print("Total number of bits is: " + str(total_bits))
-    num_ones_array = [] 
-    split_bin_ascii = wrap(bin_ascii, 256) # split in 256 bits per line
-    for i in split_bin_ascii: # calculate number of 'ones' in each of the 256bits lines
+    #print("Total number of bits is: " + str(total_bits)) 
+    split_bin_ascii = wrap(bin_ascii, 2048) # split in 2048 bits per line - 1 second
+    for i in split_bin_ascii: # calculate number of 'ones' in each of the 2048 bits lines
         num_ones_array.append(i.count('1'))
+    #print("Here are the bits splites: " + str(split_bin_ascii))
+    #print("Number of ones in each line: " + str(num_ones_array))
 
-    print("Here are the bits splites: " + str(split_bin_ascii))
-    print("Number of ones in each line: " + str(num_ones_array))
 
-# elapsed_time = timeit.timeit(code, number=100)/100 # time function 100 times
-# print(elapsed_time)
+fileBinToAscii()
+
+binSheet = pd.DataFrame()                       # Array to Pandas Column
+binSheet['Ones'] = num_ones_array
+binSheet.dropna(inplace = True)
+binSheet = binSheet.reset_index()
+binSheet['index'] = binSheet['index'] + 1
+binSheet = binSheet.rename(columns = {'index': 'Time'})
+binSheet['Sum'] = binSheet['Ones'].cumsum()
+binSheet['Average'] = binSheet['Sum']/(binSheet['Time'])
+binSheet['Zscore'] = (binSheet['Average']-1024)/(22.62741699796/(binSheet['Time']**0.5))
+
+file_to_save = "file.xlsx"      #data_file.replace(".bin", ".xlsx")
+#data_file2 = os.path.basename(data_file)
+#data_file2 = data_file2.replace(".csv", "")
+number_rows = len(binSheet.Time)
+writer = pd.ExcelWriter(file_to_save, engine='xlsxwriter')
+binSheet.to_excel(writer,sheet_name='Z-Test',index=False)
+workbook = writer.book
+worksheet = writer.sheets['Z-Test']
+chart = workbook.add_chart({'type': 'line'})
+chart.set_title({
+'name': 'Z-Score: ',
+'name_font': {
+    'name': 'Calibri',
+    'color': 'black',
+    },
+})
+
+chart.set_x_axis({
+'name': 'Time',
+'name_font': {
+    'name': 'Calibri',
+    'color': 'black'
+    },
+'num_font': {
+    'name': 'Calibri',
+    'color': 'black',
+    },
+})
+
+chart.set_y_axis({
+'name': 'Z-Score',
+'name_font': {
+    'name': 'Calibri',
+    'color': 'black'
+    },
+'num_font': {
+    'color': 'black',
+    },
+})
+
+chart.set_legend({'position': 'none'})
+chart.add_series({'values': ['Z-Test', 1, 4, number_rows, 4],
+                  'categories': ['Z-Test', 1, 0, number_rows, 0]})
+worksheet.insert_chart('G2', chart)
+writer.save()
+#tk.messagebox.showinfo('File Saved','Salvo em ' + file_to_save)
+print(binSheet)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#elapsed_time = timeit.timeit(fileBinToAscii, number=1)/1 # time function 100 times
+#print(elapsed_time)
